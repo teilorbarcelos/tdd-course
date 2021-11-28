2583e05629ed301ca7d82b88c8dc3460f0caab20
Teilor Souza Barcelos <tsb-@live.com>
2021-11-28 13:12:45 -0300

    class 9 finished

--- cypress/integration/ui/devbook.spec.js
+++ cypress/integration/ui/devbook.spec.js
@@ -3,4 +3,14 @@ describe('devbook application', () => {
     cy.visit('http://localhost:3000/')
     cy.get('h2[data-test="heading"]').contains('DevBook')
   })
+
+  it('shows a book list', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('div[data-test="bookList"]').should('exist')
+    cy.get('div.bookItem').should((books) => {
+      expect(books).to.have.length(2)
+      const titles = [...books].map(book => book.querySelector('h2').innerHTML)
+      expect(titles).to.deep.equal(['Refactoring', 'Domain-driven design'])
+    })
+  })
 })
--- src/App.js
+++ src/App.js
@@ -9,6 +9,14 @@ function App() {
         component="h2"
         data-test="heading"
       >DevBook</Typography>
+      <div data-test="bookList">
+        <div className="bookItem">
+          <h2 className="title">Refactoring</h2>
+        </div>
+        <div className="bookItem">
+          <h2 className="title">Domain-driven design</h2>
+        </div>
+      </div>
     </>
   )
 }
