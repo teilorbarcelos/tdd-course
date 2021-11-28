d12d6afaaa9cadb0c06749b766be4cff9f91878f
Teilor Souza Barcelos <tsb-@live.com>
2021-11-28 15:30:02 -0300

    class 13 finished

--- cypress/integration/ui/devbook.spec.js
+++ cypress/integration/ui/devbook.spec.js
@@ -1,4 +1,26 @@
+import { api } from "../../../src/services/api"
+
 describe('devbook application', () => {
+  before(() => {
+    return api.delete('/books?_cleanup=true').catch((err) => err)
+  })
+
+  beforeEach(() => {
+    const books = [
+      { 'name': 'Refactoring', 'id': 1 },
+      { 'name': 'Domain-driven design', 'id': 2 },
+      { 'name': 'Building microsservices', 'id': 3 }
+    ]
+
+    return books.map(book => api.post('/books', book, {
+      headers: { 'Content-type': 'application/json' }
+    }))
+  })
+
+  afterEach(() => {
+    return api.delete('/books?_cleanup=true').catch((err) => err)
+  })
+
   it('visits the devbook', () => {
     cy.visit('http://localhost:3000/')
     cy.get('h2[data-test="heading"]').contains('DevBook')
@@ -8,9 +30,9 @@ describe('devbook application', () => {
     cy.visit('http://localhost:3000/')
     cy.get('div[data-test="bookList"]').should('exist')
     cy.get('div.bookItem').should((books) => {
-      expect(books).to.have.length(2)
+      expect(books).to.have.length(3)
       const titles = [...books].map(book => book.querySelector('h5').innerHTML)
-      expect(titles).to.deep.equal(['Refactoring', 'Domain-driven design'])
+      expect(titles).to.deep.equal(['Refactoring', 'Domain-driven design', 'Building microsservices'])
     })
   })
 })
